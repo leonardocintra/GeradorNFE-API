5e06da19b99aa3fbdd459d8ed3a698d8b2c609f1
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2016-07-30 13:26:18 -0300

    criado resetante das tabelas

--- geradornf/admin.py
+++ geradornf/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from geradornf.models import Emitente
+from geradornf.models import Emitente, Destinatario
 
 class EmitenteAdmin(admin.ModelAdmin):
 	list_display = ('nome_fantasia', 'nome_razao', )
 
+class DestinatarioAdmin(admin.ModelAdmin):
+	list_display = ('nome_razao', 'cnpj', )
+
 admin.site.register(Emitente, EmitenteAdmin)
+admin.site.register(Destinatario, DestinatarioAdmin)
--- geradornf/models.py
+++ geradornf/models.py
@@ -17,6 +17,7 @@ class Destinatario(models.Model):
     uf = models.CharField(max_length=2)
     pais_codigo = models.IntegerField(default=1058)
     pais = models.CharField(max_length=200)
+    data_cadasttro = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'destinatario'
@@ -41,8 +42,49 @@ class Emitente(models.Model):
     cnae = models.CharField(max_length=50)
     pais_codigo = models.IntegerField(default=1058)
     pais = models.CharField(max_length=200, default='Brasil')
-
+    data_cadasttro = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'emitente'
         ordering = ('nome_fantasia', 'nome_razao', )
+
+
+class Produto(models.Model):
+    CFOP = models.IntegerField()
+    EAN = models.CharField(max_length=50, blank=True, null=True)
+    NCM = models.IntegerField()
+    descricao = models.CharField(max_length=300)
+    unidade = models.CharField(max_length=2) #fazer um choice
+    valor_unitario = models.DecimalField(max_digits=9, decimal_places=2) 
+    data_cadasttro = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'produto'
+        ordering = ('descricao', )
+
+
+class Transportador(models.Model):
+    frete_por_conta = models.BooleanField(default=False)
+    cnpf_cnpj = models.CharField(max_length=14)
+    inscricao_estadual = models.CharField(max_length=20)
+    nome_razao = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200)
+    cidade_codigo = models.IntegerField(blank=True, null=True)
+    cidade = models.CharField(max_length=200)
+    uf = models.CharField(max_length=2)
+    data_cadasttro = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'transportador'
+        ordering = ('nome_razao', )
+
+
+class TransportadorContrato(models.Model):
+    transportador_id = models.ForeignKey(Transportador, on_delete=models.CASCADE)
+    valor_servico = models.DecimalField(decimal_places=2, max_digits=9)
+    valor_base = models.DecimalField(decimal_places=2, max_digits=9)
+    aliquota = models.DecimalField(decimal_places=2, max_digits=9)
+    valor_total = models.DecimalField(decimal_places=2, max_digits=9)
+    CFOP = models.CharField(max_length=50, blank=True, null=True)
+    cidade_codigo_placa = models.IntegerField(blank=True, null=True)
+    RNTC = models.CharField(max_length=50, blank=True, null=True)
